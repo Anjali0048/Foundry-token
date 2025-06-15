cbd5283dd1334d86ab0e176a448ec50863853de0
Anjali Singh <anjalisingh1373@gmai.com>
2025-06-15 03:58:12 +0000

    tests and RevertTests

--- src/FLTToken.sol
+++ src/FLTToken.sol
@@ -12,24 +12,10 @@ contract FLTToken is ERC20 {
 
     function mint(address to, uint256 amount) public {
         // require(msg.sender == owner, "Only owner can mint tokens");
-        _mint(to, amount * 10 ** decimals());
-    }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        require(amount > 0, "Transfer amount must be greater than zero");
-        return super.transfer(to, amount * 10 ** decimals());
-    }
-
-    function approveFn(address spender, uint256 amount) public returns (bool) {
-        require(amount > 0, "Approval amount must be greater than zero");
-        return super.approve(spender, amount * 10 ** decimals());
+        _mint(to, amount);
     }
 
     function burn(uint256 amount) public {
-        _burn(msg.sender, amount * 10 ** decimals());
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return super.totalSupply();
+        _burn(msg.sender, amount);
     }
 }
--- test/FLTToken.t.sol
+++ test/FLTToken.t.sol
@@ -12,38 +12,46 @@ contract TestFLTToken is Test {
         t.mint(address(this), 50);
     }
 
-    function amount(uint256 amt) public returns (uint256) {
-        return amt * 10 ** t.decimals();
-    }
-
     function testMint() public {
-        assertEq(t.balanceOf(address(this)), amount(50), "Initial supply should be 50 tokens");
+        assertEq(t.balanceOf(address(this)), 50, "Initial supply should be 50 tokens");
         assertEq(t.balanceOf(address(0x1234)), 0, "OK");
     }
 
     function testTransfer() public {
         address addr = address(0x1234);
         t.transfer(addr, 15);
-        assertEq(t.balanceOf(addr), amount(15), "OK");
-        assertEq(t.balanceOf(address(this)), amount(35), "OK");
+        assertEq(t.balanceOf(addr), 15, "OK");
+        assertEq(t.balanceOf(address(this)), 35, "OK");
 
         vm.prank(addr);
         t.transfer(address(this), 10);
-        assertEq(t.balanceOf(addr), amount(5), "OK");
-        assertEq(t.balanceOf(address(this)), amount(45), "OK");
-
+        assertEq(t.balanceOf(addr), 5, "OK");
+        assertEq(t.balanceOf(address(this)), 45, "OK");
     }
 
     function testAprroveFn() public {        
         address addr = address(0x1234);
 
-        t.approveFn(addr, 20);
-        assertEq(t.allowance(address(this), addr), amount(20), "OK");
+        t.approve(addr, 20);
+        assertEq(t.allowance(address(this), addr), 20, "OK");
 
         vm.prank(addr);
-        t.transferFrom(address(this), address(0x67890), amount(10));
-        assertEq(t.balanceOf(address(this)), amount(40), "OK");
-        assertEq(t.balanceOf(address(0x67890)), amount(10), "OK");
-
+        t.transferFrom(address(this), address(0x67890), 10);
+        assertEq(t.balanceOf(address(0x67890)), 10, "0x67890 has 10 tokens left");
+        assertEq(t.balanceOf(address(this)), 40, "this address has 40 tokens left");
     }
+
+    function test_RevertWhen_InsuffAllow() public {
+
+        address addr = address(0x1234);
+        t.approve(addr, 10);
+
+        vm.prank(addr);
+
+        vm.expectRevert();
+        t.transferFrom(address(this), address(0x67890), 20);
+        assertEq(t.balanceOf(address(0x67890)), 0, "0x67890 should not have any tokens");
+    }
+
+    
 }
